29e7b30090b25f9455bb76f28a9a606714b55cf9
Musty-16 <abassmustapha31@gmail.com>
2026-06-08 12:48:26 +0100

    changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,35 @@
 export const projects = [
   {
     id: 1,
-    title: "Analytics Dashboard",
-    description: "Real-time data visualization suite that reduced client reporting time by 40%.",
-    tech: ["React", "D3.js", "Express", "PostgreSQL"],
-    liveUrl: "https://analytics-demo.vercel.app",
-    githubUrl: "https://github.com/yourusername/analytics-dashboard",
-    image: "/hero.png"
+    title: "Travel Smart",
+    description: "Developed a travel information platform that enables users to explore destinations, track live flights,access travel reviews, and learn about diverse cultures and customs.",
+    tech: ["React", "JavaScript","HTML", "CSS", "JSON"],
+    liveUrl: "https://travelsmart-vertex-coders-techwiz5.netlify.app/",
+    image: "assets/TravelSmart.png",
   },
   {
     id: 2,
-    title: "TaskFlow API",
-    description: "RESTful API with JWT auth and rate limiting, handling 10k+ daily requests.",
-    tech: ["Node.js", "Express", "MongoDB", "Jest"],
-    liveUrl: "https://taskflow-api.vercel.app",
-    githubUrl: "https://github.com/yourusername/taskflow-api",
-    image: null
+    title: "Campus Connect",
+    description: "Built a modern campus management platform for discovering events, connecting students andfaculty, and sharing important updates",
+    tech: [ "React.js", "Vite", "React Router", "CSS", "Git"],
+    liveUrl: "https://campusconnect-stacksmith-techwiz6.netlify.app/",
+    githubUrl: "https://github.com/Bezliab/campusconnect.git",
+    image: "assets/CampusConnect.png",
   },
   {
     id: 3,
-    title: "Portfolio Site",
+    title: "My-Portfolio Site",
     description: "High-contrast, Geist-font portfolio built with React and deployed on Vercel.",
     tech: ["React", "Vite", "Tailwind"],
+    liveUrl: "https://yourname.vercel.app",
+    githubUrl: "https://github.com/yourusername/portfolio",
+    image: null
+  },
+  {
+    id: 4,
+    title: "CurrenSee",
+    description: " Currency Conversion & Market Tracker.Designed a Flutter-based mobile application providing real-time currency conversion, historicalexchange-rate visualization, market insights, customizable rate alerts, and secure userauthentication. Implemented responsive Material 3 UI and scalable architecture",
+    tech: ["FLutter", "Dart"],
     liveUrl: "https://yourname.vercel.app",
     githubUrl: "https://github.com/yourusername/portfolio",
     image: null
